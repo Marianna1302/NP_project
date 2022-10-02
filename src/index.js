c5678c5343972ad71f47b153d4353ea9b9a57769
Марианна Игнашкина <maritiger130201@gmail.com>
2022-10-03 00:54:49 +0300

    Create a city list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {BrowserRouter} from 'react-router-dom';
+import Office from './components/Pages/Office';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 ReactDOM.render(
+  
     <BrowserRouter>
         <App />
     </BrowserRouter>, document.getElementById('root'));
+
     
 
